7568287d44712e876472ab61bb8ed7f135854370
Blackrush <blackrushx@gmail.com>
2014-03-11 17:46:10 +0100

    adds then method to NetworkCommand

--- network/src/main/java/org/rocket/network/NetworkCommand.java
+++ network/src/main/java/org/rocket/network/NetworkCommand.java
@@ -8,4 +8,33 @@ public interface NetworkCommand {
 
 	void async();
 	void async(Duration max);
+
+    default NetworkCommand then(NetworkCommand other) {
+        NetworkCommand self = this;
+        return new NetworkCommand() {
+            @Override
+            public void now() {
+                self.now();
+                other.now();
+            }
+
+            @Override
+            public void now(Duration max) {
+                self.now(max);
+                other.now(max);
+            }
+
+            @Override
+            public void async() {
+                self.async();
+                other.async();
+            }
+
+            @Override
+            public void async(Duration max) {
+                self.async(max);
+                other.async(max);
+            }
+        };
+    }
 }
