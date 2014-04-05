d9f5bcfb3d953ad0d31ebbf01fd5d66de26d4a0f
Blackrush <blackrushx@gmail.com>
2014-04-05 12:10:48 +0200

    a NetworkService can now broadcast to an arbitrary subset of clients

--- network/src/main/java/org/rocket/network/NetworkService.java
+++ network/src/main/java/org/rocket/network/NetworkService.java
@@ -5,13 +5,18 @@ import net.engio.mbassy.bus.IMessageBus;
 import org.rocket.Service;
 import org.rocket.network.event.NetworkEvent;
 
+import java.util.stream.Stream;
+
 public interface NetworkService<C extends NetworkClient> extends Service {
 	int getActualConnectedClients();
 	int getMaxConnectedClients();
 
 	ImmutableSet<C> getClients();
 
-	NetworkCommand broadcast(Object o);
+	NetworkCommand broadcast(Stream<C> clients, Object o);
+	default NetworkCommand broadcast(Object o) {
+		return broadcast(getClients().stream(), o);
+	}
 
 	IMessageBus<NetworkEvent<C>, ?> getEventBus();
 }
