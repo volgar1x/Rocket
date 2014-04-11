f609377bc8db8378715bf083df366225e7934ce5
Blackrush <blackrushx@gmail.com>
2014-04-11 16:36:20 +0200

    adds some shortcut methods

--- network/src/main/java/org/rocket/network/NetworkClient.java
+++ network/src/main/java/org/rocket/network/NetworkClient.java
@@ -1,5 +1,8 @@
 package org.rocket.network;
 
+import org.fungsi.Unit;
+import org.fungsi.concurrent.Future;
+
 import java.net.SocketAddress;
 import java.util.function.Consumer;
 
@@ -10,4 +13,28 @@ public interface NetworkClient {
 	NetworkCommand write(Object o);
 	NetworkCommand transaction(Consumer<Transactional> fn);
 	NetworkCommand close();
+
+    default void send(Object o) {
+        write(o).now();
+    }
+
+    default Future<Unit> cast(Object o) {
+        return write(o).async();
+    }
+
+    default void transact(Consumer<Transactional> fn) {
+        transaction(fn).now();
+    }
+
+    default Future<Unit> commit(Consumer<Transactional> fn) {
+        return transaction(fn).async();
+    }
+
+    default void kickNow() {
+        close().now();
+    }
+
+    default Future<Unit> kick() {
+        return close().async();
+    }
 }
