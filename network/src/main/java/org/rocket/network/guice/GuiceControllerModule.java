d935c8d6d18fbc0a11ee52a9263d6d3fc96c2721
Blackrush <blackrushx@gmail.com>
2014-07-31 00:06:15 +0200

    fix guice controller module

--- network/src/main/java/org/rocket/network/guice/GuiceControllerModule.java
+++ network/src/main/java/org/rocket/network/guice/GuiceControllerModule.java
@@ -1,24 +1,77 @@
 package org.rocket.network.guice;
 
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.PrivateBinder;
+import com.google.inject.Provider;
 import com.google.inject.binder.LinkedBindingBuilder;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.util.Types;
 import org.rocket.guice.RocketModule;
 import org.rocket.network.Controller;
+import org.rocket.network.ControllerFactory;
+import org.rocket.network.NetworkClient;
+
+import javax.inject.Inject;
+import java.util.Set;
 
 public abstract class GuiceControllerModule extends RocketModule {
+    @SuppressWarnings("unchecked")
+    private static final Key<Set<Object>> CONTROLLERS_KEY = (Key<Set<Object>>) Key.get(Types.setOf(Object.class), Controller.class);
+
+    private final Key<NetworkClient> clientKey;
+    private PrivateBinder theBinder;
     private Multibinder<Object> multibinder;
+
+    public GuiceControllerModule(Key<NetworkClient> clientKey) {
+        this.clientKey = clientKey;
+    }
+
+    public GuiceControllerModule() {
+        this(Key.get(NetworkClient.class));
+    }
+
+    private void configureControllers() {
+        theBinder.bind(Hook.class).asEagerSingleton();
+        theBinder.bind(clientKey).toProvider(Hook.class);
+        theBinder.bind(ControllerFactory.class).to(Hook.class);
+        theBinder.expose(ControllerFactory.class);
+    }
 
     @Override
     protected void before() {
-        multibinder = Multibinder.newSetBinder(binder(), Object.class, Controller.class);
+        theBinder = newPrivateBinder();
+        multibinder = Multibinder.newSetBinder(theBinder, Object.class, Controller.class);
     }
 
     @Override
     protected void after() {
+        configureControllers();
         multibinder = null;
+        theBinder = null;
     }
 
     protected LinkedBindingBuilder<Object> newController() {
         return multibinder.addBinding();
+    }
+
+    private static class Hook implements Provider<NetworkClient>, ControllerFactory {
+        @Inject private Injector injector;
+        final ThreadLocal<NetworkClient> client = new ThreadLocal<>();
+
+        @Override
+        public NetworkClient get() {
+            return client.get();
+        }
+
+        @Override
+        public synchronized Set<Object> create(NetworkClient client) {
+            this.client.set(client);
+            try {
+                return injector.getInstance(CONTROLLERS_KEY);
+            } finally {
+                this.client.remove();
+            }
+        }
     }
 }
---
+++ network/src/test/java/org/rocket/network/guice/GuiceControllerModuleTest.java
@@ -0,0 +1,58 @@
+package org.rocket.network.guice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.rocket.network.ControllerFactory;
+import org.rocket.network.NetworkClient;
+
+import javax.inject.Inject;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+public class GuiceControllerModuleTest {
+
+    @Inject ControllerFactory factory;
+
+    @org.rocket.network.Controller
+    public static class Controller {
+        @Inject NetworkClient client;
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        Injector injector = Guice.createInjector(
+                new GuiceControllerModule() {
+                    @Override
+                    protected void configure() {
+                        newController().to(Controller.class);
+                    }
+                }
+        );
+        injector.injectMembers(this);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        factory = null;
+    }
+
+    @Test
+    public void testCreateControllers() throws Exception {
+        // given
+        NetworkClient client = mock(NetworkClient.class);
+
+        // when
+        Set<Object> controllers = factory.create(client);
+
+        // then
+        assertEquals("number of controllers", 1, controllers.size());
+
+        Controller controller = (Controller) controllers.iterator().next();
+        assertEquals("controller's client", client, controller.client);
+    }
+}
