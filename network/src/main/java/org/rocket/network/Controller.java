426af1cf0801a58495a39eb4eb20ae2426398570
Blackrush <blackrushx@gmail.com>
2014-07-30 16:12:17 +0200

    Controller annotation must be a Qualifier

--- network/src/main/java/org/rocket/network/Controller.java
+++ network/src/main/java/org/rocket/network/Controller.java
@@ -1,8 +1,10 @@
 package org.rocket.network;
 
+import javax.inject.Qualifier;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Qualifier
 public @interface Controller {
 }
