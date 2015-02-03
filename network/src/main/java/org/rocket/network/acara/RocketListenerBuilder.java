9e01e490ea01fb6b1a275de937eadc27cd47bdd8
Blackrush <blackrushx@gmail.com>
2015-02-03 20:37:05 +0100

    Warn user of invalid controller action

--- network/src/main/java/org/rocket/network/acara/RocketListenerBuilder.java
+++ network/src/main/java/org/rocket/network/acara/RocketListenerBuilder.java
@@ -9,23 +9,35 @@ import org.rocket.network.Receive;
 import org.rocket.network.Supervise;
 import org.rocket.network.event.ReceiveEvent;
 import org.rocket.network.event.SuperviseEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.stream.Stream;
 
 final class RocketListenerBuilder extends JavaListenerBuilder {
+    private static final Logger logger = LoggerFactory.getLogger(RocketListenerBuilder.class);
     public static final RocketListenerBuilder instance = new RocketListenerBuilder();
 
     @Override
     protected Stream<Listener> scan(Object o, Method method) {
         if (method.isAnnotationPresent(Connect.class) || method.isAnnotationPresent(Disconnect.class)) {
+            if (method.getParameterCount() != 0) {
+                Annotation ann = method.getAnnotation(Connect.class);
+                if (ann == null) {
+                    ann = method.getAnnotation(Disconnect.class);
+                }
+                logger.warn("{} is annotated with {} but has an invalid signature", method, ann);
+                return Stream.empty();
+            }
             boolean disconnecting = method.isAnnotationPresent(Disconnect.class);
             EventMetadata metadata = new Events.ConnectEventMetadata(disconnecting);
             Listener listener = new ConnectEventListener(metadata, o, method);
             return Stream.of(listener);
         } else if (method.isAnnotationPresent(Receive.class)) {
             if (method.getParameterCount() != 1) {
-                // TODO warn user
+                logger.warn("{} is annotated with org.rocket.network.Receive but has an invalid signature", method);
                 return Stream.empty();
             }
 
@@ -35,7 +47,7 @@ final class RocketListenerBuilder extends JavaListenerBuilder {
             return Stream.of(listener);
         } else if (method.isAnnotationPresent(Supervise.class)) {
             if (method.getParameterCount() != 1) {
-                // TODO warn user
+                logger.warn("{} is annotated with org.rocket.network.Supervise but has an invalid signature", method);
                 return Stream.empty();
             }
 
