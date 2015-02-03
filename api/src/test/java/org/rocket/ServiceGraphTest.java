38fa22608b70109ce8014b2ea6895c102b01d523
Blackrush <blackrushx@gmail.com>
2015-02-03 19:46:02 +0100

    Test ServiceGraph even more

--- api/src/test/java/org/rocket/ServiceGraphTest.java
+++ api/src/test/java/org/rocket/ServiceGraphTest.java
@@ -6,12 +6,13 @@ import org.rocket.Services.Graph;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.spy;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 import static org.rocket.ServicePath.absolute;
 
 public class ServiceGraphTest {
@@ -21,37 +22,17 @@ public class ServiceGraphTest {
         return Services.newGraphInternal(res);
     }
 
-    class MockService implements Service {
-        final ServicePath path, dependency;
-        MockService(ServicePath path, ServicePath dependency) {
-            this.path = path;
-            this.dependency = dependency;
-        }
-
-        @Override
-        public ServicePath path() {
-            return path;
-        }
-
-        @Override
-        public ServicePath dependsOn() { return dependency; }
-
-        @Override public void start(StartReason reason) { }
-
-        @Override public void stop() { }
-    }
-
-    class A extends MockService {
-        A() { super(absolute("A"), null); }
-    }
-
-    class B extends MockService {
-        B() { super(absolute("B"), absolute("A")); }
+    Service mockService(ServicePath path, @Nullable ServicePath dep) {
+        Service service = mock(Service.class);
+        when(service.path()).thenReturn(path);
+        when(service.dependsOn()).thenReturn(dep);
+        return service;
     }
 
     @Test
     public void testNewGraph() throws Exception {
-        Service a = spy(new A()), b = spy(new B());
+        Service a = mockService(absolute("A"), null),
+                b = mockService(absolute("B"), absolute("A"));
 
         Graph graph = newGraph(a, b);
 
@@ -67,27 +48,89 @@ public class ServiceGraphTest {
     }
 
     @Test
+    public void testFold() throws Exception {
+        Service a = mockService(absolute("A"), null),
+                b = mockService(absolute("B"), absolute("A"));
+
+        Graph graph = newGraph(a, b);
+        Service service = graph.fold();
+
+        service.start(StartReason.NORMAL);
+        service.stop();
+
+        InOrder o = inOrder(a, b);
+        o.verify(a).start(StartReason.NORMAL);
+        o.verify(b).start(StartReason.NORMAL);
+        o.verify(b).stop();
+        o.verify(a).stop();
+        o.verifyNoMoreInteractions();
+    }
+
+    @Test
     public void testRewire() throws Exception {
-        Service a = spy(new A()), b = spy(new B());
+        Service a = mockService(absolute("A"), null),
+                b = mockService(absolute("B"), absolute("A"));
 
         Graph graph = newGraph(a, b);
 
-        Graph graph1 = graph.get(a.getClass());
-        Graph graph2 = graph.get(b.getClass());
+        Graph graph1 = graph.get(absolute("A"));
+        Graph graph2 = graph.get(absolute("B"));
         assert graph1 != null;
         assert graph2 != null;
 
+        // root -> A -> B
         assertThat(graph1.parent, equalTo(graph));
         assertThat(graph2.parent, equalTo(graph1));
 
-        graph.rewire(b.getClass(), null);
+        graph.rewire(absolute("B"), null);
 
+        // root -> A
+        // root -> B
         assertThat(graph1.parent, equalTo(graph));
         assertThat(graph2.parent, equalTo(graph));
 
-        graph.rewire(a.getClass(), b.getClass());
+        graph.rewire(absolute("A"), absolute("B"));
 
+        // root -> B -> A
         assertThat(graph1.parent, equalTo(graph2));
         assertThat(graph2.parent, equalTo(graph));
+
+        graph.rewire(absolute("UNKNOWN SERVICE"), null);
+
+        // root -> B -> A
+        assertThat(graph1.parent, equalTo(graph2));
+        assertThat(graph2.parent, equalTo(graph));
+
+        try {
+            graph.rewire(absolute("A"), absolute("UNKNOWN SERVICE"));
+            fail("successfully rewired to an unknown service");
+        } catch (NoSuchElementException ignored) {
+
+        }
+        // root -> B -> A
+        assertThat(graph1.parent, equalTo(graph2));
+        assertThat(graph2.parent, equalTo(graph));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCircularGraph() throws Exception {
+        Service a = mockService(absolute("A"), absolute("B")),
+                b = mockService(absolute("B"), absolute("A"));
+
+        newGraph(a, b);
+    }
+
+    @Test
+    public void testRoot() throws Exception {
+        Service a = mockService(absolute("A"), null),
+                b = mockService(absolute("B"), absolute("A"));
+
+        Graph graph = newGraph(a, b);
+
+        Graph bottomest = graph.get(absolute("B"));
+        assert bottomest != null;
+        Graph root = bottomest.root();
+
+        assertThat(root, equalTo(graph));
     }
 }
