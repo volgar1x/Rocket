8b6130228474cba918d1e530dcd7330f429acf9c
Blackrush <blackrushx@gmail.com>
2015-02-25 15:20:37 +0100

    Bring back the Prop, dammit!

--- network/src/main/java/org/rocket/network/guice/ControllerModule.java
+++ network/src/main/java/org/rocket/network/guice/ControllerModule.java
@@ -3,12 +3,13 @@ package org.rocket.network.guice;
 import com.google.inject.Key;
 import com.google.inject.Provider;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.util.Types;
 import org.rocket.guice.RocketModule;
 import org.rocket.network.*;
+import org.rocket.network.props.PropIds;
 
-import javax.inject.Inject;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -48,18 +49,6 @@ public abstract class ControllerModule extends RocketModule {
 
     protected void newController(Class<?> klass) {
         controllerMultibinder.addBinding().to(klass);
-
-        for (Field field : klass.getFields()) {
-            if (!field.isAnnotationPresent(Inject.class) && !field.isAnnotationPresent(com.google.inject.Inject.class)) {
-                continue;
-            }
-
-            if (!Prop.class.isAssignableFrom(field.getType())) {
-                continue;
-            }
-
-            newProp(keyFor(field));
-        }
     }
 
     @SuppressWarnings("unchecked")
@@ -74,36 +63,30 @@ public abstract class ControllerModule extends RocketModule {
         bind(wrap(key, MutProp.class)).toProvider(provider);
     }
 
-    private Key<?> keyFor(Field field) {
-        // find the first eligible annotation other than @Inject
-        // com.google.inject.Key only supports a single marker annotation
-        // a contrario of PropId which can handle any number of annotations
-        Annotation acc = null;
-        for (Annotation ann : field.getAnnotations()) {
-            if (Inject.class.isAssignableFrom(ann.annotationType())) {
-                continue;
-            }
-            if (com.google.inject.Inject.class.isAssignableFrom(ann.annotationType())) {
-                continue;
-            }
-
-            acc = ann;
-            break;
-        }
-
-        if (acc != null) {
-            return Key.get(field.getGenericType(), acc);
-        }
-
-        return Key.get(field.getGenericType());
+    protected void newProp(Class<?> klass) {
+        newProp(Key.get(klass));
     }
 
-    private Key<?> wrap(Key<?> key, Class<?> wrapper) {
-        throw new Error("todo");
+    protected void newProp(Class<?> klass, Annotation ann) {
+        newProp(Key.get(klass, ann));
+    }
+
+    protected void newProp(Class<?> klass, Class<? extends Annotation> ann) {
+        newProp(Key.get(klass, ann));
+    }
+
+    private Key<?> wrap(Key<?> key, Type wrapper) {
+        return key.ofType(Types.newParameterizedType(wrapper, key.getTypeLiteral().getType()));
     }
 
     private PropId asPropId(Key<?> key) {
-        throw new Error("todo");
+        if (key.getAnnotation() == null) {
+            return PropIds.type(key.getTypeLiteral().getType());
+        }
+        return PropIds.of(
+            PropIds.type(key.getTypeLiteral().getType()),
+            PropIds.annotation(key.getAnnotation())
+        );
     }
 
     private static class PropProvider implements Provider {
