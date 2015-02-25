ac0f2c67c89ee89dc05b9d39c6a67d19224f7ec3
Blackrush <blackrushx@gmail.com>
2015-02-25 19:08:26 +0100

    Slight changement in the way PropValidator behave

--- network/src/main/java/org/rocket/network/PropValidator.java
+++ network/src/main/java/org/rocket/network/PropValidator.java
@@ -1,5 +1,18 @@
 package org.rocket.network;
 
-public interface PropValidator<T> {
-    void validate(Prop<T> prop);
+import com.google.common.collect.ImmutableList;
+
+public interface PropValidator {
+    void validate(NetworkClient client);
+
+    public static PropValidator aggregate(ImmutableList<PropValidator> validators) {
+        return new PropValidator() {
+            @Override
+            public void validate(NetworkClient client) {
+                for (PropValidator validator : validators) {
+                    validator.validate(client);
+                }
+            }
+        };
+    }
 }
--- network/src/main/java/org/rocket/network/props/PropPresenceValidator.java
+++ network/src/main/java/org/rocket/network/props/PropPresenceValidator.java
@@ -1,11 +1,21 @@
 package org.rocket.network.props;
 
-import org.rocket.network.Prop;
+import org.rocket.network.MutProp;
+import org.rocket.network.NetworkClient;
+import org.rocket.network.PropId;
 import org.rocket.network.PropValidator;
 
-public class PropPresenceValidator implements PropValidator<Object> {
+public class PropPresenceValidator implements PropValidator {
+    private final PropId pid;
+
+    public PropPresenceValidator(PropPresence annotation) {
+        this.pid = PropIds.type(annotation.value());
+    }
+
     @Override
-    public void validate(Prop<Object> prop) {
+    public void validate(NetworkClient client) {
+        MutProp<Object> prop = client.getProp(pid);
+
         if (!prop.isDefined()) {
             throw new AssertionError(String.format(
                     "Prop %s must have a value",
